acfafe311be9340d67c500a8ba8e1e61656e03e0
Jhon Brown <sefa.pehlivan@hepsiburada.com>
2025-07-11 17:01:56 +0300

    fix

--- client/controller_grpc.pb.go
+++ client/controller_grpc.pb.go
@@ -19,11 +19,13 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	ControllerService_RegisterController_FullMethodName   = "/client.ControllerService/RegisterController"
-	ControllerService_GetClientLocation_FullMethodName    = "/client.ControllerService/GetClientLocation"
-	ControllerService_SetClientLocation_FullMethodName    = "/client.ControllerService/SetClientLocation"
-	ControllerService_ForwardCommand_FullMethodName       = "/client.ControllerService/ForwardCommand"
-	ControllerService_RequestClientRefresh_FullMethodName = "/client.ControllerService/RequestClientRefresh"
+	ControllerService_RegisterController_FullMethodName     = "/client.ControllerService/RegisterController"
+	ControllerService_GetClientLocation_FullMethodName      = "/client.ControllerService/GetClientLocation"
+	ControllerService_SetClientLocation_FullMethodName      = "/client.ControllerService/SetClientLocation"
+	ControllerService_ForwardCommand_FullMethodName         = "/client.ControllerService/ForwardCommand"
+	ControllerService_RequestClientRefresh_FullMethodName   = "/client.ControllerService/RequestClientRefresh"
+	ControllerService_IsControllerRegistered_FullMethodName = "/client.ControllerService/IsControllerRegistered"
+	ControllerService_BulkSetClientLocations_FullMethodName = "/client.ControllerService/BulkSetClientLocations"
 )
 
 // ControllerServiceClient is the client API for ControllerService service.
@@ -39,6 +41,8 @@ type ControllerServiceClient interface {
 	// ForwardCommand forwards a command to the appropriate controller
 	ForwardCommand(ctx context.Context, in *ForwardCommandRequest, opts ...grpc.CallOption) (*ForwardCommandResponse, error)
 	RequestClientRefresh(ctx context.Context, in *ClientRefreshRequest, opts ...grpc.CallOption) (*ClientRefreshResponse, error)
+	IsControllerRegistered(ctx context.Context, in *IsControllerRegisteredRequest, opts ...grpc.CallOption) (*IsControllerRegisteredResponse, error)
+	BulkSetClientLocations(ctx context.Context, in *BulkSetClientLocationsRequest, opts ...grpc.CallOption) (*BulkSetClientLocationsResponse, error)
 }
 
 type controllerServiceClient struct {
@@ -99,6 +103,26 @@ func (c *controllerServiceClient) RequestClientRefresh(ctx context.Context, in *
 	return out, nil
 }
 
+func (c *controllerServiceClient) IsControllerRegistered(ctx context.Context, in *IsControllerRegisteredRequest, opts ...grpc.CallOption) (*IsControllerRegisteredResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(IsControllerRegisteredResponse)
+	err := c.cc.Invoke(ctx, ControllerService_IsControllerRegistered_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *controllerServiceClient) BulkSetClientLocations(ctx context.Context, in *BulkSetClientLocationsRequest, opts ...grpc.CallOption) (*BulkSetClientLocationsResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(BulkSetClientLocationsResponse)
+	err := c.cc.Invoke(ctx, ControllerService_BulkSetClientLocations_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ControllerServiceServer is the server API for ControllerService service.
 // All implementations must embed UnimplementedControllerServiceServer
 // for forward compatibility.
@@ -112,6 +136,8 @@ type ControllerServiceServer interface {
 	// ForwardCommand forwards a command to the appropriate controller
 	ForwardCommand(context.Context, *ForwardCommandRequest) (*ForwardCommandResponse, error)
 	RequestClientRefresh(context.Context, *ClientRefreshRequest) (*ClientRefreshResponse, error)
+	IsControllerRegistered(context.Context, *IsControllerRegisteredRequest) (*IsControllerRegisteredResponse, error)
+	BulkSetClientLocations(context.Context, *BulkSetClientLocationsRequest) (*BulkSetClientLocationsResponse, error)
 	mustEmbedUnimplementedControllerServiceServer()
 }
 
@@ -136,6 +162,12 @@ func (UnimplementedControllerServiceServer) ForwardCommand(context.Context, *For
 }
 func (UnimplementedControllerServiceServer) RequestClientRefresh(context.Context, *ClientRefreshRequest) (*ClientRefreshResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RequestClientRefresh not implemented")
+}
+func (UnimplementedControllerServiceServer) IsControllerRegistered(context.Context, *IsControllerRegisteredRequest) (*IsControllerRegisteredResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method IsControllerRegistered not implemented")
+}
+func (UnimplementedControllerServiceServer) BulkSetClientLocations(context.Context, *BulkSetClientLocationsRequest) (*BulkSetClientLocationsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method BulkSetClientLocations not implemented")
 }
 func (UnimplementedControllerServiceServer) mustEmbedUnimplementedControllerServiceServer() {}
 func (UnimplementedControllerServiceServer) testEmbeddedByValue()                           {}
@@ -248,6 +280,42 @@ func _ControllerService_RequestClientRefresh_Handler(srv interface{}, ctx contex
 	return interceptor(ctx, in, info, handler)
 }
 
+func _ControllerService_IsControllerRegistered_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IsControllerRegisteredRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ControllerServiceServer).IsControllerRegistered(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ControllerService_IsControllerRegistered_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ControllerServiceServer).IsControllerRegistered(ctx, req.(*IsControllerRegisteredRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ControllerService_BulkSetClientLocations_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(BulkSetClientLocationsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ControllerServiceServer).BulkSetClientLocations(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ControllerService_BulkSetClientLocations_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ControllerServiceServer).BulkSetClientLocations(ctx, req.(*BulkSetClientLocationsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // ControllerService_ServiceDesc is the grpc.ServiceDesc for ControllerService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -274,6 +342,14 @@ var ControllerService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "RequestClientRefresh",
 			Handler:    _ControllerService_RequestClientRefresh_Handler,
+		},
+		{
+			MethodName: "IsControllerRegistered",
+			Handler:    _ControllerService_IsControllerRegistered_Handler,
+		},
+		{
+			MethodName: "BulkSetClientLocations",
+			Handler:    _ControllerService_BulkSetClientLocations_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
